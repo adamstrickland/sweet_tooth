34bb64a69f651deb14f3253858a761ed5b49c005
Adam Strickland <adam.strickland@gmail.com>
2018-10-09 17:05:55 -0500

    adds TLD-based root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "us#index", constraints: ->(r){ r.domain(2) =~ /somedomain\.com/ }
+  root to: "uk#index", constraints: ->(r){ r.domain(2) =~ /somedomain\.co\.uk/ }
   get '/us', to: "us#index"
   get '/uk', to: "uk#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
