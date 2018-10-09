fde417a4871b6a6bfc35076b154ff5f4ac4ef964
Adam Strickland <adam.strickland@gmail.com>
2018-10-09 18:53:42 -0500

    builds two react 'apps'

---
+++ app/javascript/components/UkGateway.js
@@ -0,0 +1,11 @@
+import React from "react"
+import PropTypes from "prop-types"
+class UkGateway extends React.Component {
+  render () {
+    return (
+      <h1>UK</h1>
+    );
+  }
+}
+
+export default UkGateway
