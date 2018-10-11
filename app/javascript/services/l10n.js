54f6ae75338190060f872f83816ee9066b2f7733
Adam Strickland <adam.strickland@gmail.com>
2018-10-11 13:33:29 -0500

    loads l10n file from yaml

--- app/javascript/services/l10n.js
+++ app/javascript/services/l10n.js
@@ -1,5 +1,6 @@
 import LocalizedStrings from 'react-localization';
 
-var data = require("../data/l10n.json")
+//works
+var data = require("json-loader!yaml-loader!../data/l10n.json")
 let strings = new LocalizedStrings(data);
 export default strings
