8d6aad3121bf326f15026cf9c17663dd74be1c20
Adam Strickland <adam.strickland@gmail.com>
2018-10-11 14:58:06 -0500

    moves l10n data to /config

--- app/javascript/components/Gateway.js
+++ app/javascript/components/Gateway.js
@@ -1,16 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 import strings from "../services/l10n"
-// import LocalizedStrings from 'react-localization';
-
-// let strings = new LocalizedStrings({
-//   en:{
-//     how_it_works: "Looking for that new sweater for the season? Sign up, fill out a profile, and we take care of the rest!"
-//   },
-//   en_GB:{
-//     how_it_works: "Chuffed for a new jumper for the season? Sign up, fill out a profile, and we take care of the rest!"
-//   }
-// })
 
 class Gateway extends React.Component {
   constructor(props) {
