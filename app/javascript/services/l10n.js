9710974a9019bc3466b472a6689910074150df30
Adam Strickland <adam.strickland@gmail.com>
2018-10-11 13:48:07 -0500

    works w/ yml file

--- app/javascript/services/l10n.js
+++ app/javascript/services/l10n.js
@@ -1,6 +1,6 @@
 import LocalizedStrings from 'react-localization';
 
 //works
-var data = require("json-loader!yaml-loader!../data/l10n.json")
+var data = require("json-loader!yaml-loader!../data/l10n.yml")
 let strings = new LocalizedStrings(data);
 export default strings
