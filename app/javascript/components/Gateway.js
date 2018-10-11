341b4353d71b2a0faf41d03b472793b58abaab1a
Adam Strickland <adam.strickland@gmail.com>
2018-10-11 14:26:10 -0500

    works loading l10n from api

--- app/javascript/components/Gateway.js
+++ app/javascript/components/Gateway.js
@@ -1,22 +1,25 @@
 import React from "react"
 import PropTypes from "prop-types"
-import strings from "../services/l10n"
-// import LocalizedStrings from 'react-localization';
-
-// let strings = new LocalizedStrings({
-//   en:{
-//     how_it_works: "Looking for that new sweater for the season? Sign up, fill out a profile, and we take care of the rest!"
-//   },
-//   en_GB:{
-//     how_it_works: "Chuffed for a new jumper for the season? Sign up, fill out a profile, and we take care of the rest!"
-//   }
-// })
+import l10n from "../services/l10n"
 
 class Gateway extends React.Component {
   constructor(props) {
     super(props);
 
-    strings.setLanguage(props.locale || "en");
+    this.state = {
+      strings: {}
+    }
+  }
+
+  componentWillMount() {
+    l10n.then((ls) => {
+      var strings = ls
+      strings.setLanguage(this.props.locale || "en")
+
+      this.setState({
+        strings: strings
+      });
+    })
   }
 
   render () {
@@ -26,7 +29,7 @@ class Gateway extends React.Component {
         <br/>
         <h2>How It Works</h2>
         <div>
-          {strings.how_it_works}
+          {this.state.strings.how_it_works}
         </div>
         <br/>
         <h3>Register Now!</h3>
--- app/javascript/services/l10n.js
+++ app/javascript/services/l10n.js
@@ -1,5 +1,6 @@
 import LocalizedStrings from 'react-localization';
 
-var data = fetch("/api/l10n").then(res => res.json())
-let strings = new LocalizedStrings(data);
+var strings = fetch("/api/l10n")
+             .then((res) => res.json())
+             .then((json) => new LocalizedStrings(json))
 export default strings
